054d03b6990f38f4d9bec1f38cb4e2682c3e2ee0
Daniel Huhn <daniel_huhn@outlook.com>
2014-12-27 19:53:38 +0100

    refactored the send notification method

--- notifications/notification.js
+++ notifications/notification.js
@@ -1,16 +1,18 @@
 var request = require('request');
 var db = require(__dirname+'/../db');
 var settings = require(__dirname+'/../settings');
+var log = require(__dirname+'/../log');
 
 function Notification() {
   var self = this;
 
-  self.send = function(receiverIds, msg, callback) {
+  self.send = function(receiverIds, action, params, callback) {
     var gcmUrl = 'https://android.googleapis.com/gcm/send';
     var req = {
       registration_ids: receiverIds,
       data: {
-        msg: msg
+        action: action,
+        params: params
       }
     }
 
@@ -26,16 +28,23 @@ function Notification() {
     }
 
     request(options, function(err, res, body){
-      debugger;
+      if(err){
+        log.err('error sending push notification: '+err);
+        return;
+      }
+
       callback();
     })
   }
 
-  self.sendToAll = function(msg, callback) {
+  self.sendToAll = function(action, params, callback) {
     db.registrations.find(function(err, users){
+      if(err) throw err;
       users.toArray(function(err, usersArray){
+        if(err) throw err;
+
         var ids = usersArray.map(function(u){ return u.gcmId});
-        self.send(ids, msg, callback);
+        self.send(ids, action, params, callback);
       })
     })
   }
