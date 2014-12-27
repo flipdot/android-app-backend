dbeffeb180935446932bbb35f701e1636b07c67f
Daniel Huhn <daniel_huhn@outlook.com>
2014-12-27 14:39:34 +0100

    added notification class to send notifications to the gcm servers

---
+++ notifications/notification.js
@@ -0,0 +1,39 @@
+var request = require('request');
+var db = require(__dirname+'/../db');
+
+function Notification() {
+  var self = this;
+
+  self.send = function(receiverIds, msg, callback) {
+    var gcmUrl = 'https://android.googleapis.com/gcm/send';
+    var req = {
+      registration_ids: receiverIds,
+      data: {
+        msg: msg
+      }
+    }
+
+    var options = {
+      url: gcmUrl,
+      method: 'POST',
+      body: req,
+      json: true
+    }
+
+    request(options, function(err, res, body){
+      debugger;
+      callback();
+    })
+  }
+
+  self.sendToAll = function(msg, callback) {
+    db.registrations.find(function(err, users){
+      users.toArray(function(err, usersArray){
+        var ids = usersArray.map(function(u){ return u.gcmId});
+        self.send(ids, msg, callback);
+      })
+    })
+  }
+}
+
+module.exports = new Notification();
